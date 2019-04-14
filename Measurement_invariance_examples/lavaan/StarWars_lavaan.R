efdfe373fe7a3505a5ea24cf98c8c39c0cf7b180
Sacha Epskamp <mail@sachaepskamp.com>
2019-04-14 19:13:39 +0200

    MI now via markdown

--- Measurement_invariance_examples/lavaan/StarWars_lavaan.R
+++ Measurement_invariance_examples/lavaan/StarWars_lavaan.R
@@ -1,33 +1,34 @@
-# Load packages:
-library("dplyr") # I always load this
+#' Load packages:
+library("dplyr")
 library("lavaan")
 
-# Read the data:
+#' Read the data:
 Data <- read.csv("StarWars.csv", sep = ",")
 
-# This data encodes the following variables:
-# Q1: I am a huge Star Wars fan! (star what?)
-# Q2: I would trust this person with my democracy.
-# Q3: I enjoyed the story of Anakin's early life.
-# Q4: The special effects in this scene are awful (Battle of Geonosis).
-# Q5: I would trust this person with my life.
-# Q6: I found Darth Vader'ss big reveal in "Empire" one of the greatest moments in movie history.
-# Q7: The special effects in this scene are amazing (Death Star Explosion).
-# Q8: If possible, I would definitely buy this droid.
-# Q9: The story in the Star Wars sequels is an improvement to the previous movies.
-# Q10: The special effects in this scene are marvellous (Starkiller Base Firing).
-# Q11: What is your gender?
-# Q12: How old are you?
-# Q13: Have you seen any of the Star Wars movies?
+#' This data encodes the following variables:
+#' 
+#'   - Q1: I am a huge Star Wars fan! (star what?)
+#'   - Q2: I would trust this person with my democracy.
+#'   - Q3: I enjoyed the story of Anakin's early life.
+#'   - Q4: The special effects in this scene are awful (Battle of Geonosis).
+#'   - Q5: I would trust this person with my life.
+#'   - Q6: I found Darth Vader'ss big reveal in "Empire" one of the greatest moments in movie history.
+#'   - Q7: The special effects in this scene are amazing (Death Star Explosion).
+#'   - Q8: If possible, I would definitely buy this droid.
+#'   - Q9: The story in the Star Wars sequels is an improvement to the previous movies.
+#'   - Q10: The special effects in this scene are marvellous (Starkiller Base Firing).
+#'   - Q11: What is your gender?
+#'   - Q12: How old are you?
+#'   - Q13: Have you seen any of the Star Wars movies?
 
-# Let's say we are interested in seeing if people >= 30 like the original trilogy better than people < 30.
-# First we can make a grouping variable:
+#' Let's say we are interested in seeing if people >= 30 like the original trilogy better than people < 30.
+#' First we can make a grouping variable:
 Data$agegroup <- ifelse(Data$Q12 < 30, "young", "less young")
 
-# Let's look at the distribution:
-table(Data$agegroup) # Pretty even...
+#' Let's look at the distribution:
+table(Data$agegroup) #' Pretty even...
 
-# Test for configural invariance:
+#' Test for configural invariance:
 Model <- '
 Prequels =~ Q2 + Q3 + Q4 + Q1
 Original =~ Q5 + Q6 + Q7 + Q1
@@ -36,56 +37,57 @@ Q4 ~~ Q10
 '
 conf <- cfa(Model, Data, group = "agegroup")
 
-# Look at fit:
-conf # P > 0.05 so we do not reject exact fit!
-fitMeasures(conf) # Looks good!
+#' Look at fit:
+conf #' P > 0.05 so we do not reject exact fit!
+fitMeasures(conf) #' Looks good!
 
-# Test for weak invariance:
+#' Test for weak invariance:
 weak <- cfa(Model, Data, group = "agegroup",
             group.equal = "loadings")
 
-# Compare groups:
-anova(conf,weak) # Weak variance is accepted
+#' Compare groups:
+anova(conf,weak) #' Weak variance is accepted
 
-# Test for strong invariance:
+#' Test for strong invariance:
 strong <- cfa(Model, Data, group = "agegroup",
             group.equal = c("loadings","intercepts"))
 
-# Compare groups:
-anova(conf,weak, strong) # Questionable... 
+#' Compare groups:
+anova(conf,weak, strong) #' Questionable... 
 
-# What do the residuals tell us?
-residuals(strong) # Some misfit in Q8 and Q10 in both groups
+#' What do the residuals tell us?
+residuals(strong) #' Some misfit in Q8 and Q10 in both groups
 
-# And the lagrange multiplier tests?
+#' And the lagrange multiplier tests?
 lavTestScore(strong)$uni %>% arrange(-X2)
-# Tells me .p37 and .p39., which parameters are those:
 
-parameterEstimates(strong)
-# Intercepts for Q8 and Q10, Q10 is the highest
+#' Tells me .p37 and .p39., which parameters are those:
 
-# Lets' try freeing up intercept for Q10 on special effects (this is also reasonable, it could be that older
-# people judge special effects on different criteria than younger people).
+parameterEstimates(strong)[37:39,]
+#' Intercepts for Q8 and Q10, Q10 is the highest
+
+#' Lets' try freeing up intercept for Q10 on special effects (this is also reasonable, it could be that older
+#' people judge special effects on different criteria than younger people).
 strong_partial <- cfa(Model, Data, group = "agegroup",
               group.equal = c("loadings","intercepts"),
               group.partial = "Q10 ~ 1")
 
-# Compare:
+#' Compare:
 anova(conf,weak, strong_partial) 
-# The partial strong invariance model can be accepted
+#' The partial strong invariance model can be accepted
 
-# Test for strict invariance:
+#' Test for strict invariance:
 strict <- cfa(Model, Data, group = "agegroup",
               group.equal = c("loadings","intercepts","residuals",
               "residual.covariances"),
               group.partial = "Q10 ~ 1")
 
-# Compare:
+#' Compare:
 anova(conf,weak, strong_partial, strict) 
-# Strict invariance can be accepted!
+#' Strict invariance can be accepted!
 
-### Now we can test for homogeneity! ###
-# Are the variances and covariances equal?
+#' ### Now we can test for homogeneity! 
+#' Are the variances and covariances equal?
 eqvars <- cfa(Model, Data, group = "agegroup",
               group.equal = c("loadings","intercepts","residuals",
                               "residual.covariances",
@@ -93,10 +95,10 @@ eqvars <- cfa(Model, Data, group = "agegroup",
               group.partial = "Q10 ~ 1")
 
 
-# Compare:
-anova(strict, eqvars)  # Acceptable
+#' Compare:
+anova(strict, eqvars)  
 
-# What about the means?
+#' This is acceptable. What about the means?
 eqvars_and_means <- cfa(Model, Data, group = "agegroup",
               group.equal = c("loadings","intercepts","residuals",
                               "residual.covariances",
@@ -104,10 +106,10 @@ eqvars_and_means <- cfa(Model, Data, group = "agegroup",
                               "means"),
               group.partial = "Q10 ~ 1")
 
-# Compare:
-anova(strict, eqvars, eqvars_and_means)  # Rejected!
+#' Compare:
+anova(strict, eqvars, eqvars_and_means) 
 
-# Let's look at the means:
-parameterEstimates(eqvars)[82:84, ] # I looked before and saw rows 82-84 are the ones of interest
+#' Homogenous means can be rejected! Let's look at the means:
+parameterEstimates(eqvars)[82:84, ] #' I looked before and saw rows 82-84 are the ones of interest
 
-# Interestingly, People >= 30 seem to like the prequels more and the sequels less than people < 30!
+#' Interestingly, People >= 30 seem to like the prequels more and the sequels less than people < 30!
--- Measurement_invariance_examples/psychonetrics/StarWars_psychonetrics.R
+++ Measurement_invariance_examples/psychonetrics/StarWars_psychonetrics.R
@@ -1,143 +1,143 @@
-# Load packages:
-library("dplyr") # I always load this
+#' Load packages:
+library("dplyr") 
 
-# Install psychonetrics devel version:
+#' Install psychonetrics devel version:
 library("devtools")
 install_github("sachaepskamp/psychonetrics")
 
-# Load psychonetrics:
+#' Load psychonetrics:
 library("psychonetrics")
 
-# Read the data:
+#' Read the data:
 Data <- read.csv("StarWars.csv", sep = ",")
 
-# This data encodes the following variables:
-# Q1: I am a huge Star Wars fan! (star what?)
-# Q2: I would trust this person with my democracy.
-# Q3: I enjoyed the story of Anakin's early life.
-# Q4: The special effects in this scene are awful (Battle of Geonosis).
-# Q5: I would trust this person with my life.
-# Q6: I found Darth Vader'ss big reveal in "Empire" one of the greatest moments in movie history.
-# Q7: The special effects in this scene are amazing (Death Star Explosion).
-# Q8: If possible, I would definitely buy this droid.
-# Q9: The story in the Star Wars sequels is an improvement to the previous movies.
-# Q10: The special effects in this scene are marvellous (Starkiller Base Firing).
-# Q11: What is your gender?
-# Q12: How old are you?
-# Q13: Have you seen any of the Star Wars movies?
-
-# Let's say we are interested in seeing if people >= 30 like the original trilogy better than people < 30.
-# First we can make a grouping variable:
+#' This data encodes the following variables:
+#' 
+#'   - Q1: I am a huge Star Wars fan! (star what?)
+#'   - Q2: I would trust this person with my democracy.
+#'   - Q3: I enjoyed the story of Anakin's early life.
+#'   - Q4: The special effects in this scene are awful (Battle of Geonosis).
+#'   - Q5: I would trust this person with my life.
+#'   - Q6: I found Darth Vader'ss big reveal in "Empire" one of the greatest moments in movie history.
+#'   - Q7: The special effects in this scene are amazing (Death Star Explosion).
+#'   - Q8: If possible, I would definitely buy this droid.
+#'   - Q9: The story in the Star Wars sequels is an improvement to the previous movies.
+#'   - Q10: The special effects in this scene are marvellous (Starkiller Base Firing).
+#'   - Q11: What is your gender?
+#'   - Q12: How old are you?
+#'   - Q13: Have you seen any of the Star Wars movies?
+#' 
+#' Let's say we are interested in seeing if people >= 30 like the original trilogy better than people < 30.
+#' First we can make a grouping variable:
 Data$agegroup <- ifelse(Data$Q12 < 30, "young", "less young")
 
-# Let's look at the distribution:
-table(Data$agegroup) # Pretty even...
+#' Let's look at the distribution:
+table(Data$agegroup) #' Pretty even...
 
-# Let's look at the mean scores:
+#' Observed variables:
+obsvars <- paste0("Q",1:10)
+
+#' Let's look at the mean scores:
 Data %>% group_by(agegroup) %>% summarize_each_(funs(mean),vars = obsvars)
-# Less young people seem to score higher on prequel questions and lower on other questions
+#' Less young people seem to score higher on prequel questions and lower on other questions
 
-# Factor loadings matrix:
+#' Factor loadings matrix:
 Lambda <- matrix(0, 10, 3)
 Lambda[1:4,1] <- 1
 Lambda[c(1,5:7),2] <- 1
 Lambda[c(1,8:10),3] <- 1
 
-# Residual covariances:
+#' Residual covariances:
 Theta <- diag(1, 10)
 Theta[4,10] <- Theta[10,4] <- 1
 
-# Observed variables:
-obsvars <- paste0("Q",1:10)
-
-
-# Latents:
+#' Latents:
 latents <- c("Prequels","Original","Sequels")
 
-# Make model:
-mod_configural <- lvm(Data, lambda = Lambda, vars = obsvars, 
-            latents = latents, sigma_epsilon = Theta, 
+#' Make model:
+mod_configural <- lvm(Data, lambda = Lambda, vars = obsvars,
+            latents = latents, sigma_epsilon = Theta,
             identification = "variance",
             groups =  "agegroup")
 
-# Run model:
+#' Run model:
 mod_configural <- mod_configural %>% runmodel
 
-# Look at fit:
-mod_configural 
+#' Look at fit:
+mod_configural
 mod_configural %>% fit
 
-# Looks good, let's try weak invariance:
+#' Looks good, let's try weak invariance:
 mod_weak <- mod_configural %>% groupequal("lambda") %>% runmodel
 
-# Compare models:
+#' Compare models:
 compare(configural = mod_configural, weak = mod_weak)
 
-# weak invariance can be accepted, let's try strong:
+#' weak invariance can be accepted, let's try strong:
 mod_strong <- mod_weak %>% groupequal("tau") %>% runmodel
-# Means are automatically identified
+#' Means are automatically identified
 
-# Compare models:
+#' Compare models:
 compare(configural = mod_configural, weak = mod_weak, strong = mod_strong)
 
-# Questionable p-value and AIC difference, but ok BIC difference. This is quite good, but let's take a look. 
-# I have not yet implemented LM tests for equality constrains, but we can loko at something called "equality-free" MIs:
+#' Questionable p-value and AIC difference, but ok BIC difference. This is quite good, but let's take a look.
+#' I have not yet implemented LM tests for equality constrains, but we can loko at something called "equality-free" MIs:
 mod_strong %>% MIs(matrices = "tau", type = "free")
 
-# Indicates that Q10 would improve fit. We can also look at residuals:
+#' Indicates that Q10 would improve fit. We can also look at residuals:
 residuals(mod_strong)
 
-# Let's try freeing intercept 10:
+#' Let's try freeing intercept 10:
 mod_strong_partial <- mod_strong %>% groupfree("tau",10) %>% runmodel
 
-# Compare all models:
-compare(configural = mod_configural, 
+#' Compare all models:
+compare(configural = mod_configural,
         weak = mod_weak,
-        strong = mod_strong, 
+        strong = mod_strong,
         strong_partial = mod_strong_partial)
 
-# This seems worth it and lead to an acceptable model! It seems that older people find the latest special effects more marvellous!
+#' This seems worth it and lead to an acceptable model! It seems that older people find the latest special effects more marvellous!
 mod_strong_partial %>% getmatrix("tau")
 
-# Now let's investigate strict invariance:
+#' Now let's investigate strict invariance:
 mod_strict <- mod_strong_partial %>% groupequal("sigma_epsilon") %>% runmodel
 
-# Compare all models:
-compare(configural = mod_configural, 
+#' Compare all models:
+compare(configural = mod_configural,
         weak = mod_weak,
         strong_partial = mod_strong_partial,
         strict = mod_strict)
-# Strict invariance can be accepted!
+#' Strict invariance can be accepted!
 
-### Now we can test for homogeneity! 
-# Are the latent variances equal?
+#'  Now we can test for homogeneity!
+#' Are the latent variances equal?
 mod_eqvar <- mod_strict %>% groupequal("sigma_zeta") %>% runmodel
 
-# Compare:
-compare(strict = mod_strict, eqvar = mod_eqvar) # Acceptable
+#' Compare:
+compare(strict = mod_strict, eqvar = mod_eqvar) 
 
-# What about the means? (alpha = tau_eta)
+#' This is acceptable. What about the means? (alpha = tau_eta)
 mod_eqmeans <- mod_eqvar %>% groupequal("tau_eta") %>% runmodel
 
-# Compare:
+#' Compare:
 compare(eqvar = mod_eqvar, eqmeans = mod_eqmeans)
 
-# Rejected! We could look at MIs again:
+#' Rejected! We could look at MIs again:
 mod_eqmeans %>% MIs(matrices = "tau_eta", type = "free")
 
-# Indicates the strongest effect for prequels. Let's see what happens:
+#' Indicates the strongest effect for prequels. Let's see what happens:
 eqmeans2 <- mod_eqvar %>% groupequal("tau_eta",row = c("Original","Sequels")) %>% runmodel
 
-# Compare:
+#' Compare:
 compare(eqvar = mod_eqvar, eqmeans = eqmeans2)
-# Questionable, what about the sequels as well?
+#' Questionable, what about the sequels as well?
 
 eqmeans3 <- mod_eqvar %>% groupequal("tau_eta", row = "Original") %>% runmodel
 
-# Compare:
-compare(eqvar = mod_eqvar, eqmeans = eqmeans3) 
+#' Compare:
+compare(eqvar = mod_eqvar, eqmeans = eqmeans3)
 
-# Still questionable.. Let's look at the mean differences:
+#' Still questionable.. Let's look at the mean differences:
 mod_eqvar %>% getmatrix("tau_eta")
 
-# Looks like people over 30 like the prequels better and the other two trilogies less!
+#' Looks like people over 30 like the prequels better and the other two trilogies less!
